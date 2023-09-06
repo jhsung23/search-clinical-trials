af1ca310ff236c4fe546db168ca44728b46656b4
sungjihyun <jhsung23@gmail.com>
2023-09-06 13:27:04 +0900

    feat: app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 import '@/App.css';
 
 function App() {
-  return <Outlet />;
+  return (
+    <Main>
+      <Outlet />
+    </Main>
+  );
 }
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 15%;
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  background-color: #cae9ff;
+`;
 
 export default App;
